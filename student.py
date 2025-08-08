93de6f7ace550c2ed015f1b70a49b62081f83269
Chathura <prabashana.dev@gmail.com>
2025-08-08 08:56:22 -0700

    update the student class

--- student.py
+++ student.py
@@ -1,17 +1,24 @@
 class student:
-    def __init__(self, stuID, stuName, stuAge):
-        self.stuID = stuID
-        self.stuName = stuName
-        self.stuAge = stuAge
+    def __init__(self):
+        self.stuID = ""
+        self.stuName = ""
+        self.stuAge = ""
 
-student1 = student("001", "Chathura", "23")
-student2 = student("002", "Akash", "22")
-student3 = student("003", "Samidu", "21")
+    def setstudent(self, id, name, age):
+        self.stuID = id
+        self.stuName = name
+        self.stuAge = age
 
-students = [student1, student2, student3]
+    def showstudent(self):
+        print(f"Student ID: {self.stuID}, Name: {self.stuName}, Age: {self.stuAge}")
 
-for student in students:
-    print(f"Student ID: {student.stuID} Name: {student.stuName}, Age: {student.stuAge}")
+student1 = student()
+student1.setstudent("001", "Chathura", "23")
+student2 = student()
+student2.setstudent("002", "Akash", "22")
+student3 = student()
+student3.setstudent("003", "Samidu", "21")
 
-
-        
+student1.showstudent()
+student2.showstudent()
+student3.showstudent()
