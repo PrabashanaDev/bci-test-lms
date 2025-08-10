c2f41200147fa3cfe4f52d6a923e4d3b56e781e7
Chathura <prabashana.dev@gmail.com>
2025-08-10 10:17:28 -0700

    update student class

--- student.py
+++ student.py
@@ -12,13 +12,3 @@ class Student:
     def showStudent(self):
         print(f"Student ID: {self.stuID}, Name: {self.stuName}, Age: {self.stuAge}")
 
-student1 = Student()
-student1.setStudent("001", "Chathura", "23")
-student2 = Student()
-student2.setStudent("002", "Akash", "22")
-student3 = Student()
-student3.setStudent("003", "Samidu", "21")
-
-student1.showStudent()
-student2.showStudent()
-student3.showStudent()
