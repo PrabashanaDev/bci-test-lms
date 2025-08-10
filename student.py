abd3a23703fd962ab220416e1cc1ebd368941702
Chathura <prabashana.dev@gmail.com>
2025-08-10 05:08:28 -0700

    Fixing Student class errors

--- student.py
+++ student.py
@@ -1,24 +1,24 @@
-class student:
+class Student:
     def __init__(self):
         self.stuID = ""
         self.stuName = ""
         self.stuAge = ""
 
-    def setstudent(self, id, name, age):
+    def setStudent(self, id, name, age):
         self.stuID = id
         self.stuName = name
         self.stuAge = age
 
-    def showstudent(self):
+    def showStudent(self):
         print(f"Student ID: {self.stuID}, Name: {self.stuName}, Age: {self.stuAge}")
 
-student1 = student()
-student1.setstudent("001", "Chathura", "23")
-student2 = student()
-student2.setstudent("002", "Akash", "22")
-student3 = student()
-student3.setstudent("003", "Samidu", "21")
+student1 = Student()
+student1.setStudent("001", "Chathura", "23")
+student2 = Student()
+student2.setStudent("002", "Akash", "22")
+student3 = Student()
+student3.setStudent("003", "Samidu", "21")
 
-student1.showstudent()
-student2.showstudent()
-student3.showstudent()
+student1.showStudent()
+student2.showStudent()
+student3.showStudent()
