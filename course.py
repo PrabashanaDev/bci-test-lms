75c3ab5d9c270d0cf3f621c42c03144f5de0f6e7
Chathura <prabashana.dev@gmail.com>
2025-08-10 10:14:30 -0700

    data abstraction to course class

--- course.py
+++ course.py
@@ -1,4 +1,6 @@
-class Course:
+from abc import ABC, abstractmethod
+
+class Course(ABC):
     def __init__(self):
         self.course_id = ""
         self.course_name = ""
@@ -10,17 +12,27 @@ class Course:
     def showCourse(self):
         print(f"Course ID: {self.courID}, Course Name: {self.courName}")
 
+    @abstractmethod
+    def calSalary(self):
+        pass
+
 
 class Degree(Course):
     def __init__(self):
+        pass
+    def calSalary(self):
         pass
 
 class diploma(Course):
     def __init__(self):
         pass
+    def calSalary(self):
+        pass
 
 class certificate():
     def __init__(self):
         pass
-    
+    def calSalary(self):
+        pass
+
 
