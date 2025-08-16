c741a6f14de20613faf7953a434f7546b6e58da7
Chathura <prabashana.dev@gmail.com>
2025-08-16 05:53:50 -0700

    Code Update

--- course.py
+++ course.py
@@ -1,13 +1,10 @@
 from abc import ABC, abstractmethod
 
 class Course(ABC):
-    def __init__(self):
-        self.course_id = ""
-        self.course_name = ""
-
-    def setCourse(self, courID, courName):
+    def __init__(self, courID, courName):
         self.courID = courID
         self.courName = courName
+
 
     def showCourse(self):
         print(f"Course ID: {self.courID}, Course Name: {self.courName}")
@@ -18,21 +15,43 @@ class Course(ABC):
 
 
 class Degree(Course):
-    def __init__(self):
-        pass
+    def __init__(self, degName):
+        self.degName = degName
+    def showDegree(self):
+        print(f"Degree Name: {self.degName}")
     def calAvg(self):
         pass
 
 class diploma(Course):
-    def __init__(self):
-        pass
+    def __init__(self, dipName):
+        self.dipName = dipName
+    def showDiploma(self):
+        print(f"Diploma Name: {self.dipName}")
     def calAvg(self):
         pass
 
 class certificate():
-    def __init__(self):
-        pass
+    def __init__(self, certName):
+        self.certName = certName
+    def showCertificate(self):
+        print(f"Certificate Name: {self.certName}")
     def calAvg(self):
         pass
+
+degree1 = Degree("BSIT")
+degree2 = Degree("BSSE")
+degree1.showDegree()
+degree2.showDegree()
+
+diploma1 = diploma("Aquinas")
+diploma1.showDiploma()
+
+certificate1 = certificate("Robotic")
+certificate1.showCertificate()
+
+
+
+
+
 
 
--- student.py
+++ student.py
@@ -1,14 +1,18 @@
 class Student:
-    def __init__(self):
-        self.stuID = ""
-        self.stuName = ""
-        self.stuAge = ""
-
-    def setStudent(self, id, name, age):
+    def __init__(self, id, name, age):
         self.stuID = id
         self.stuName = name
         self.stuAge = age
 
+    
+
     def showStudent(self):
         print(f"Student ID: {self.stuID}, Name: {self.stuName}, Age: {self.stuAge}")
 
+student1 = Student("001", "Chathura", "23")
+student2 = Student("002", "Akash", "22")
+student3 = Student("003", "Samidu", "21")
+
+student1.showStudent()
+student2.showStudent()
+student3.showStudent()
