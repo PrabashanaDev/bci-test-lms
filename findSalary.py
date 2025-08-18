93d6cba5b20682b98f4b0b954cdf7aa4cd8aa736
Chathura <prabashana.dev@gmail.com>
2025-08-18 01:29:33 -0700

    update findSalary and Employee

--- findSalary.py
+++ findSalary.py
@@ -4,11 +4,6 @@ employee1 = Academic("001", "Chathura", 1000, 8)
 employee2 = NonAcademic("002", "Akash", 800, 8)
 employee3 = Academic("003", "Samidu", 900, 8)
 
-
-
-
-
-
 print("Academic Salary:", employee1.calculateSalary())
 print("Non Academic Salary:", employee2.calculateSalary())
 print("Academic Salary:", employee3.calculateSalary())
