5cb246779c400a345e5cb01d7528fbb0cc71f881
Chathura <prabashana.dev@gmail.com>
2025-08-18 03:03:23 -0700

    Add for loop and connection to course

--- findaverage.py
+++ findaverage.py
@@ -5,9 +5,7 @@ student1 = Student("001", "Chathura", "23")
 student2 = Student("002", "Akash", "22")
 student3 = Student("003", "Samidu", "21")
 
-student1.setCourse(Degree("BSIT"))
-student2.setCourse(certificate("BSSE"))
-student3.setCourse(diploma("Aquinas"))
+
 
 student1.stuMarks(90)
 student2.stuMarks(100)
