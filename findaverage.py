46bd069b5db67eed6c75cb7f75babfdc92eda3c0
Chathura <prabashana.dev@gmail.com>
2025-08-24 10:24:13 -0700

    Updating

--- findaverage.py
+++ findaverage.py
@@ -1,19 +1,22 @@
-from student import BciPostGraduate
+from student import BciPostGraduate, BciUnderGraduate
 
 student1 = BciPostGraduate()
-student1.setStudent("001", "Chathura", 14, "BSIT", 90, "ABC ROAD")
+student1.setStudent("001", "Chathura", "14", "BSIT", "90", "ABC ROAD")
+student1.showStudent()
 
 student2 = BciPostGraduate()
-student2.setStudent("002", "Akash Vishmitha", 23, "AE", 100, "DEF ROAD")
+student2.setStudent("002", "Akash", "23", "BSIT", "80", "Colombo ROAD")
+student2.showStudent()
 
-student3 = BciPostGraduate()
-student3.setStudent("003", "Samidu Kaushalya", 22, "R", 95, "GHI ROAD")
+student3 = BciUnderGraduate()
+student3.setStudent("003", "Samidu", "22", "BSSE", "70", "Negombo ROAD")
+student3.showStudent()
 
 students = [student1, student2, student3]
 
 student1.stuMarks(90)
 student2.stuMarks(100)
-student3.stuMarks(95)
+student3.stuMarks(80)
 
 avg = sum([s.marks for s in students]) / len(students)
 print(f"Average Marks: {avg}")
--- student.py
+++ student.py
@@ -53,10 +53,19 @@ class BciUnderGraduate(Student):
 
 if __name__ == "__main__":
 
-    Student1 = BciPostGraduate()
-    Student1.setStudent("001", "Chathura", "14", "BSIT", "90", "ABC ROAD")
+    student1 = BciPostGraduate()
+    student1.setStudent("001", "Chathura", "14", "BSIT", "90", "ABC ROAD")
+    student1.showStudent()
 
-    Student1.showStudent()
+    student2 = BciPostGraduate()
+    student2.setStudent("002", "Akash", "23", "BSIT", "80", "Colombo ROAD")
+    student2.showStudent()
+
+    student3 = BciUnderGraduate()
+    student3.setStudent("003", "Samidu", "22", "BSSE", "70", "Negombo ROAD")
+    student3.showStudent()
+
+
 
 
 
