fde326d07dcb4942842c9cc1fc78709d8dbae392
Chathura <prabashana.dev@gmail.com>
2025-08-24 08:08:33 -0700

    Updating

--- campus.py
+++ campus.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from faculty import Faculty
+from course import BciDegree,BciDiploma,BciCertificate
+from faculty import BCIFaculty
 class Campus(ABC):
     def __init__(self, campusName, campusLocation):
         self.campusName = campusName
@@ -9,7 +10,30 @@ class Campus(ABC):
     def showCampusDetails(self):
         pass
 
-class Faculty(Campus):
+print("------------------------------------------------------")
+print("Courses")
+print("------------------------------------------------------")
+
+class courses(Campus):
+    course1 = BciDegree("BSIT 1", "Information Technology")
+    course1.showCourse()
+
+    course2 = BciDiploma("AE 1", "Aquinas English")
+    course2.showCourse()
+
+    course3 = BciCertificate("R 1", "Robotics")
+    course3.showCourse()
+
+print("------------------------------------------------------")
+print("Faculties")
+print("------------------------------------------------------")
+
+class faculties(Campus):
+    Faculty1 = BCIFaculty("School of Computing", "Dr.Waruna", "001")
+    Faculty2 = BCIFaculty("School of Business", "Chathura", "002")
+    Faculty3 = BCIFaculty("School of Education", "Chathura", "003")
+    Faculty4 = BCIFaculty("School of English", "Chathura", "004")
+
     Faculty1.show_facultyNames()
     Faculty2.show_facultyNames()
     Faculty3.show_facultyNames()
